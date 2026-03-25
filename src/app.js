1f828f17fe18a5e0ad68ab9fa2cdad480798ce1a
devashish mishra <mishradevashish931@gmail.com>
2026-03-26 00:53:10 +0530

    initial commit

---
+++ src/app.js
@@ -0,0 +1,19 @@
+const express=require("express");
+const app=express(); // Instance of express js application
+
+
+app.use((req,res) => {
+    res.send("Namaste from server")
+});
+
+// GIVING ROUTE 
+
+// app.use("/test",(req,res)=>{  // "/test" -> route
+//     console.log("hello from server")
+// })
+app.listen(3000,()=>{
+    console.log(`server is successfully listening on port 3000`);
+});
+
+
+//  INSTALL NPM NODEMON TO Automatically restarts your server when files change
---
+++ src/homework.md
@@ -0,0 +1,13 @@
+- Create a repository
+- Initialize the repo
+- Learn about Node_modules ,package.json,package-lock.json
+- Install express
+- Create a server
+- Listen on port 1818
+- Write request handlers for /test ,/hello
+- Install nodemon and update scripts inside package.json
+- What are dependencies
+- What is the use of "-g" in npm install
+- Differnce between caret and tilda (^ and ~)
+
+                             
