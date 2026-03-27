1d8b0f07ac9995b69512ed172d742f9860a6259b
devashish mishra <mishradevashish931@gmail.com>
2026-03-27 16:21:59 +0530

    commit

--- src/app.js
+++ src/app.js
@@ -1,19 +1,34 @@
+// const express=require("express");
+// const app=express(); // Instance of express js application
+
+
+// app.use((req,res) => {
+//     res.send("Namaste from server") //Non rauting
+// });
+
+// // GIVING ROUTE 
+
+// // app.use("/test",(req,res)=>{  // "/test" -> route
+// //     console.log("hello from server")
+// // })
+// app.listen(3000,()=>{
+//     console.log(`server is successfully listening on port 3000`);
+// });
+
+
+ //  INSTALL NPM NODEMON TO Automatically restarts your server when files change
+
+
+
 const express=require("express");
-const app=express(); // Instance of express js application
+const app=express();
 
-
-app.use((req,res) => {
-    res.send("Namaste from server") //Non rauting
-});
-
-// GIVING ROUTE 
-
-// app.use("/test",(req,res)=>{  // "/test" -> route
-//     console.log("hello from server")
-// })
-app.listen(3000,()=>{
-    console.log(`server is successfully listening on port 3000`);
+app.use((req,res)=>{
+    console.log("hello mr.devashish");
+    //res.send("namaste jii")
 });
 
 
-//  INSTALL NPM NODEMON TO Automatically restarts your server when files change
+app.listen(4000,()=>{
+    console.log(`server is listening at http://localhost:4000`)
+})
