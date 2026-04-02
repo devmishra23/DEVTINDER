90d3b705279a9e3d065b9227b93db7f4d62d40b6
devashish mishra <mishradevashish931@gmail.com>
2026-04-02 08:13:16 +0530

    crud operation in server

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@
 
 // // GIVING ROUTE 
 
-// // app.use("/test",(req,res)=>{  // "/test" -> route
+// // app.use("/test",(req,res)=>{  // "/test" -> route --> Anything that matches after the slash "/" this raute handler follow this
 // //     console.log("hello from server")
 // // })
 // app.listen(3000,()=>{
@@ -20,15 +20,62 @@
 
 
 
+// const express=require("express");
+// const app=express();
+
+
+// app.use("/namaste", (req,res)=>{
+//         console.log("namaste from server");
+//         res.send("namaste froms server");
+//     } );
+
+// app.use((req,res)=>{
+//     console.log("hello mr.devashish");
+
+    
+//     //res.send("namaste jii")
+// });
+
+
+// app.listen(1818,()=>{
+//     console.log(`server is listening at http://localhost:1818`)
+// })
+
+
+
+
 const express=require("express");
 const app=express();
 
-app.use((req,res)=>{
-    console.log("hello mr.devashish");
-    //res.send("namaste jii")
+
+// THIS WILL ONLY HANDLE GET CALL TO /user
+app.get("/user",(req,res)=>{
+    res.send({firstName: "devashish" ,lastName:"Mishra"});
+});
+
+//This WILL ONLY HANDLE POST CALL TO /user
+
+app.post("/user",(req,res)=>{
+    console.log("Save data to the database");
+    res.send("data successfully saved");
+})
+
+
+app.delete("/user",(req,res)=>{
+    console.log("Save data to the database");
+    res.send("data successfully deleted");
+})
+
+
+// THIS WILL MATCH ALL THE API CALL TO /test
+
+app.use("/test",(req,res)=>{
+    res.send("hell0 from the server");
 });
 
 
-app.listen(4000,()=>{
-    console.log(`server is listening at http://localhost:4000`)
-})
+
+app.listen(1818,()=>{
+    console.log("Server is running at port 1818...")
+});
+
