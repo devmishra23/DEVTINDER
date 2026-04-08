7628c3794dfc3e0b4da5fb3b0742938d754791ec
devashish mishra <mishradevashish931@gmail.com>
2026-04-08 15:03:55 +0530

    multiple route handler

--- src/app.js
+++ src/app.js
@@ -44,38 +44,125 @@
 
 
 
-const express=require("express");
-const app=express();
+// const express=require("express");
+// const app=express();
 
-
+// app.use("/user",(req,res)=>{
+//     res.send("hahahahahhaa");
+// })
 // THIS WILL ONLY HANDLE GET CALL TO /user
-app.get("/user",(req,res)=>{
-    res.send({firstName: "devashish" ,lastName:"Mishra"});
-});
+// app.get("/user",(req,res)=>{
+//     res.send({firstName: "devashish" ,lastName:"Mishra"});
+// });
 
 //This WILL ONLY HANDLE POST CALL TO /user
 
-app.post("/user",(req,res)=>{
-    console.log("Save data to the database");
-    res.send("data successfully saved");
-})
+// app.post("/user",(req,res)=>{
+//     console.log("Save data to the database");
+//     res.send("data successfully saved");
+// })
 
 
-app.delete("/user",(req,res)=>{
-    console.log("Save data to the database");
-    res.send("data successfully deleted");
-})
+// app.delete("/user",(req,res)=>{
+//     console.log("Save data to the database");
+//     res.send("data successfully deleted");
+// })
 
 
-// THIS WILL MATCH ALL THE API CALL TO /test
+// // THIS WILL MATCH ALL THE API CALL TO /test
 
-app.use("/test",(req,res)=>{
-    res.send("hell0 from the server");
+// app.use("/test",(req,res)=>{
+//     res.send("hell0 from the server");
+// });
+
+
+
+// app.listen(7777,()=>{
+//     console.log("Server is running at port 7777 ...")
+// });
+
+
+// PLAYING WITH RAUTES
+
+// const express=require("express");
+// const app=express();
+
+// app.get("/user",(req,res)=>{
+//     console.log(req.query);  //Getting   QUERY PARAMS
+//     res.send({FirstName:"Devashish",LastName: "Mishra"})
+// })
+
+// app.listen(7777,()=>{
+//     console.log(`server is running at port 7777`);
+// })
+
+
+
+
+// const express=require("express");
+// const app=express();
+
+// app.get("/user",(req,res)=>{
+    // console.log(req.query);  //Getting   QUERY PARAMS
+    // res.send("Route handler")
+    
+// });
+
+// app.listen(7777,()=>{
+//     console.log(`server is running at port 7777`);
+// });
+
+
+// ROUTE handlers
+
+
+// const express=require("express");
+// const app=express();
+
+// app.use("/user",(req,res)=>{
+//     // res.send("Route handler");
+//     console.log("handling routes");
+//     //res.send("Response sending back");   //it is waiting for response
+    
+// });
+// (req,res)=>{
+//     res.send("2nd route handler");
+// }
+
+// app.listen(1818,()=>{
+//     console.log(`server is running at http://localhost:1818`);
+// })
+
+
+//  In Upper code we oberserved that we cant able to handle 2nd route so the solution of this problem is using 
+//  next();
+
+
+const express=require("express");
+const app=express();
+
+app.use("/user",
+    (req,res,next)=>{
+    // res.send("Route handler");
+    console.log("handling routes1");
+    //res.send("Response sending back");   //it is waiting for response...It always have to be comment out because if we uncomment this it may throw an error
+    next();
+    
+},
+    (req,res,next)=>{
+    console.log("handling 2nd route");
+    //res.send("2nd route handler");
+    next();
+},
+    (req,res)=>{
+    console.log("handling 3rd route");
+    res.send("3rd route handler");
+},
+    (req,res)=>{
+    console.log("handling 4th route");
+    res.send("4th route handler");
 });
-
-
 
 app.listen(1818,()=>{
-    console.log("Server is running at port 1818...")
-});
-
+    console.log(`server is running at http://localhost:1818`);
+})
